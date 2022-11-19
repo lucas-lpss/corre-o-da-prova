04928c6f46ab62ff8cf168b485858975ad0e93d9
Lucas-lpss <lcss.lopes@hotmail.com>
2022-11-18 21:41:45 -0300

    terminado

--- src/main/java/corporationLoster/bancoShow/controller/ClienteController.java
+++ src/main/java/corporationLoster/bancoShow/controller/ClienteController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(value = "/CLiente/v1")
+@RequestMapping(value = "/cliente/v1")
 public class ClienteController {
     @Autowired
     private ClienteRepository clienteRepository;
@@ -26,7 +26,7 @@ public class ClienteController {
     @ResponseBody
     public Cliente create(@RequestBody Cliente cliente){
         Cliente clienteSocializado = calcs.calc(cliente);
-        Cliente clienteSaved = clienteRepository.save(cliente);
+        Cliente clienteSaved = clienteRepository.save(clienteSocializado);
         return clienteSaved;
     }
     @GetMapping("{id}")
@@ -55,7 +55,7 @@ public class ClienteController {
 
     }
     @PutMapping(value = "update/id")
-    public String updateClienteById(@PathVariable Long id, String novoNome){
+    public String updateClienteById(@PathVariable Long id,@RequestBody String novoNome){
         Optional<Cliente> clienteOptional = clienteRepository.findById(id);
         if(clienteOptional.isPresent()){
             Cliente c = clienteOptional.get();
--- src/main/java/corporationLoster/bancoShow/utils/Calculo.java
+++ src/main/java/corporationLoster/bancoShow/utils/Calculo.java
@@ -5,15 +5,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class Calculo {
-    public Cliente calc(Cliente cliente) {
-        if (cliente.getReceita() > 10000000.00) {
+    public Cliente calc (Cliente cliente) {
+        if (cliente.getReceita() > 1000000000.00) {
             Double valorIdeal = cliente.getReceita() - 99999999.9;
             cliente.setReceitaTotal(valorIdeal);
             return cliente;
 
         } else {
             Double porcentagem = (cliente.getReceita() * 0.05);
-            cliente.setReceita(cliente.getReceita() - porcentagem);
+            cliente.setReceitaTotal(cliente.getReceita() - porcentagem);
             return cliente;
 
 
